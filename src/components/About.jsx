2986bfd97c6a6edd0b1e9ee7a2da4b0bdd8bd073
Oséas Stoterau <oseas.stoterau1@hotmail.com>
2024-03-26 19:51:33 -0300

    feat: adds more text in about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,9 +3,14 @@ const About = () => {
       <section className="about-container">
         <h2>About</h2>
         <p>
-          This is my portfolio!
+          Web Developer equipped with a diverse and promising
+          skill-set. Experienced with the latest development tools
+          and procedures. Able to effectively self-manage during
+          independent projects, as well as collaborate as part of a
+          productive team.
         </p>
         <p>
+          This is my portfolio!
           Here you will find a couple of projects I developed or was part of.
         </p>
       </section>
